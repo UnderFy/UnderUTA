b452c2d287429db26c9b7be6ee57b0e4116f1496
UnderFy <murilonovais016@gmail.com>
2026-09-01 02:36:46 -0300

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://xbdvmjjpfgwmzuzfazoy.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_ulC-CdiVzcNdiHrOncouEw_HZlVDRr6";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
